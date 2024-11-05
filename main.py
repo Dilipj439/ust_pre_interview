8cd174e745866bb4b65483d9dce3720d43125a12
unknown <dilipkumar27j@gmail.com>
2024-11-05 18:28:35 +0530

    pre-interview screening

---
+++ main.py
@@ -0,0 +1,10 @@
+from scripts import frequency_of_str, circular_queue, validity_of_password
+
+def execute_code():
+    frequency_of_str.frequency_of_string()
+    circular_queue.circular_queue_dic()
+    validity_of_password.validate_pwd()
+
+
+if __name__ == "__main__":
+    execute_code()
